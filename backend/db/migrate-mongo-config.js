19a560e12b055cf917db0c839bb1b69c4b095808
StYaroslav <yaroslavstenko@gmail.com>
2020-10-20 12:57:56 +0300

    added migrations for db

---
+++ backend/db/migrate-mongo-config.js
@@ -0,0 +1,32 @@
+// In this file you can configure migrate-mongo
+
+const configuration = require('../config')
+
+const config = {
+  mongodb: {
+    // TODO Change (or review) the url to your MongoDB:
+    url: configuration.get("mongoURI"),
+
+    // TODO Change this to your database name:
+    databaseName: configuration.get("dbName"),
+
+    options: {
+      useNewUrlParser: true, // removes a deprecation warning when connecting
+      useUnifiedTopology: true, // removes a deprecating warning when connecting
+      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+    }
+  },
+
+  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+  migrationsDir: "migrations",
+
+  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
+  changelogCollectionName: "changelog",
+
+  // The file extension to create migrations and search for in migration dir 
+  migrationFileExtension: ".js"
+};
+
+// Return the config as a promise
+module.exports = config;
